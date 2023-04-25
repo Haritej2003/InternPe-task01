8194d48e989b5eb343dbefbbc32099f2cee4c33d
Juvvagani Haritej <128915257+Haritej2003@users.noreply.github.com>
2023-04-25 17:22:58 +0530

    Add files via upload
    As a part of python internship at InternPe 
    task 01: simple calculator using Python

---
+++ Task01.py
@@ -0,0 +1,45 @@
+
+#Python Programming internship at InternPe 
+#Task 01: implementing simple calculator using python
+def add(n1,n2):
+    return n1+n2
+def sub(n1,n2):
+    return n1-n2
+def mul(n1,n2):
+    return n1*n2
+def div(n1,n2):
+    return n1/n2
+
+
+while(1):
+    
+    print("1.Addition")
+    print("2.Subtraction")
+    print("3.Multiplication")
+    print("4.Division")
+    print("5.EXIT")
+    op=int(input("choose one operation"))
+    
+    if op==1:
+       n1=int(input("Enter first number"))
+       n2=int(input("Enter second number"))
+       print(n1,"+",n2,"=",add(n1,n2))
+    elif op==2:
+        n1=int(input("Enter first number"))
+        n2=int(input("Enter second number"))
+        print(n1,"-",n2,"=",sub(n1,n2))
+    elif op==3:
+        n1=int(input("Enter first number"))
+        n2=int(input("Enter second number"))
+        print(n1,"*",n2,"=",mul(n1,n2))
+    elif op==4:
+        n1=int(input("Enter first number"))
+        n2=int(input("Enter second number"))
+        print(n1,"/",n2,"=",div(n1,n2))
+    elif op==5:
+        break
+    else:
+        print("Invalid Option")
+         
+print("EXITED")       
+         
